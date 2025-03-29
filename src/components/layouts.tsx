7ee58b95187db8772154fcb193b1966ebfc1da71
Sang Do <dovansang97@gmail.com>
2025-03-29 15:46:06 +0700

    Add layouts

---
+++ src/components/layouts.tsx
@@ -0,0 +1,16 @@
+import { AppSidebar } from './app-sidebar'
+import { Header } from './header'
+import { Providers } from './providers'
+import { SidebarInset } from './ui/sidebar'
+
+export const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Providers>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        {children}
+      </SidebarInset>
+    </Providers>
+  )
+}
